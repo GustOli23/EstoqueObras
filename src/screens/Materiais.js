b21561aaba6d2fa4228fcb1ce99ac2b678f8c062
GustOli23 <ap.gustavo21@gmail.com>
2025-11-18 13:03:59 -0300

    Correção final: ajuste na navegação, MaterialForm, Materiais e AppNavigator

--- src/screens/Materiais.js
+++ src/screens/Materiais.js
@@ -28,7 +28,7 @@ export default function Materiais() {
 
   // 🔹 Criar novo material
   const handleNewMaterial = () => {
-    navigation.navigate("MaterialFormScreen");
+    navigation.navigate("MaterialForm");
   };
 
   // 🔹 Editar material existente
@@ -39,7 +39,7 @@ export default function Materiais() {
       valor_unitario: material.valor_unitario.toFixed(2).toString(),
       estoque_minimo: material.estoque_minimo.toString(),
     };
-    navigation.navigate("MaterialFormScreen", { material: materialData });
+    navigation.navigate("MaterialForm", { material: materialData });
   };
 
   // 🔹 Filtro e busca
